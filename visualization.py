dac6a62ccaf8f24f89ddf4c6298c5742f1684101
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-04-01 16:19:12 +0200

    visualisations and readme

--- visualization.py
+++ visualization.py
@@ -1,6 +1,23 @@
 
 import matplotlib.pyplot as plt
 import scipy.signal as sg
+import seaborn as sn
+from matplotlib.colors import Normalize
+import numpy as np
+class MidpointNormalize(Normalize):
+    """
+    Auxiliar function that helps on modifying the scale of colors of a matrix to allow a better visualization
+    @Normalize: Normalize object
+    """
+    
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
 
 def _filter(values):
         #Filter parameters
@@ -16,15 +33,15 @@ def _filter(values):
         b, a = sg.butter(N, Wn, 'low')
         return sg.filtfilt(b, a, values)
 
-def plot_accuracy_over_time(classifications, label_to_id, x_ticks, filtered=False):
+def plot_accuracy_over_time(classifications, id_to_label, x_ticks, filtered=False):
     for y in classifications:
-        target = list(label_to_id.keys())[list(label_to_id.values()).index(y)]
+        target = id_to_label[int(y)]
         plt.figure(figsize=(10,7))
         plt.rcParams.update({'font.size': 32})
         plt.title(f'Accuracy {target}')
         
         for pred in range(len(classifications[y])):
-            label = list(label_to_id.keys())[list(label_to_id.values()).index(pred)]
+            label = id_to_label[pred]
             if filtered:
                 plt.plot(x_ticks, _filter(classifications[y][pred]), linewidth=5, label=label)
             else:
@@ -33,6 +50,20 @@ def plot_accuracy_over_time(classifications, label_to_id, x_ticks, filtered=Fals
         plt.ylabel('Accuracy (%)')
         plt.xlabel('Time (s)')
         plt.legend()
-        plt.savefig(f'out/img/{target}_filtered.png' if filtered else f'out/img/{target}.png', bbox_inches='tight', dpi=600)
-        plt.savefig(f'out/img/{target}_filtered.svg' if filtered else f'out/img/{target}.svg', bbox_inches='tight', dpi=600)
+        plt.savefig(f'out/figures/{target}_filtered.png' if filtered else f'out/figures/{target}.png', bbox_inches='tight', dpi=600)
+        plt.savefig(f'out/figures/{target}_filtered.svg' if filtered else f'out/figures/{target}.svg', bbox_inches='tight', dpi=600)
         plt.show()
+
+def plot_confusion_matrix(confusion_matrix, id_to_label, n_samples=1000, n_tests=100):
+    mean = np.trace(confusion_matrix)/confusion_matrix.shape[0]
+    labels = [id_to_label[y] for y in range(confusion_matrix.shape[0])]
+
+    plt.rcParams.update({'font.size': 10})
+    plt.figure(figsize=(10,7))
+    plt.suptitle('Confusion Matrix (%.2f%% accuracy, %d samples, %d tests)' % (mean, n_samples, n_tests), y=0.95, x=0.45, size=16)
+    sn.set(font_scale=1.4)
+    sn.heatmap(confusion_matrix, cmap='Blues', annot_kws={"size":12}, annot=True , xticklabels=labels, yticklabels=labels)
+    plt.xlabel("Predicted object")
+    plt.ylabel("Actual object")
+    plt.savefig(f'out/figures/confusion_matrix_{n_samples}samples_{n_tests}tests.png', bbox_inches='tight')
+    plt.show()
